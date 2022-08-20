9b19bebd8b946d4a5c0c894e1336bfff806ab703
soumya-khanna <s44khann@uwaterloo.ca>
2022-08-20 15:17:04 -0400

    template for Blacklist

--- frontend/src/views/Blacklist.js
+++ frontend/src/views/Blacklist.js
@@ -5,8 +5,8 @@ function Blacklist() {
   return (
     <div className="List Blacklist flex-col">
     <div className="List TitleSummary">
-      <h3>Your Whitelisted Sites </h3>
-      <b>These are the websites whose terms and conditions you have agreed to</b>
+      <h3>Your Blacklisted Sites </h3>
+      <b>These are the websites whose terms and conditions you have not agreed to</b>
     </div>
     <div className="List SitesContainer">
       <div className="List SiteNameContainer flex-row">
@@ -17,12 +17,12 @@ function Blacklist() {
   <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
 </svg>
 </div>
-<div className="removeIcon">
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-  <path strokeLinecap="round" strokeLinejoin="round" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+<div className="List whitelistIcon">
+<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+  <path strokeLinecap="round" strokeLinejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
 </svg>
 </div>
-<div className="blacklistIcon">
+<div className="List removeIcon">
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={3}>
   <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
 </svg>
