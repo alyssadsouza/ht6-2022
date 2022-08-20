203eeec548c3cb1c25a94d75133020e62bc1b647
soumya-khanna <s44khann@uwaterloo.ca>
2022-08-20 14:52:18 -0400

    fixes in Whitelist

--- frontend/src/views/Whitelist.js
+++ frontend/src/views/Whitelist.js
@@ -1,51 +1,7 @@
 import { useEffect, useState } from 'react';
-import AgreeMessage from "../components/Agreement/AgreeMessage";
-import AgreePrompt from "../components/Agreement/AgreePrompt";
-import PrivacyPolicy from '../components/PrivacyPolicy/PrivacyPolicy';
-import TermsAndConditions from '../components/TermsAndConditions/TermsAndConditions';
 import './List.css';
 
 function Whitelist() {
-
-  const websiteData = {
-    url: "",
-    privacy: {
-      url: "",
-      summary: ["text", "privacy"]
-    },
-    terms: {
-      url: "",
-      summary: ["terms", "and", "conditions", "terms", "and", "conditions"]
-    }
-  };
-
-  const userApi = {
-    whitelisted: ["google", "twitter"],
-    blacklisted: ["meta", "facebook"],
-  };
-
-  const [userData, setUserData] = useState({});
-
-  useEffect(() => setUserData(userApi), []);
-
-  useEffect(() => {
-    // update userApi
-    // setUserData(userApi);
-  }, [userData]);
-
-  const alreadyVisited = () => {
-    return (
-      userData?.whitelisted?.includes(websiteData?.url) ||
-      userData?.blacklisted?.includes(websiteData?.url)
-    );
-  };
-
-  const isBlacklisted = () => {
-    return userData?.blacklisted?.includes(websiteData?.url);
-  }
-  console.debug('userData', userData);
-
-
   return (
     <div className="List Whitelist flex-col">
     <div className="List TitleSummary">
