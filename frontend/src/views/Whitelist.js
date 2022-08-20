f3947995d32d68b4fc4ad13bb0d8a953f71c146a
soumya-khanna <s44khann@uwaterloo.ca>
2022-08-20 15:18:43 -0400

    Whitelist changes

--- frontend/src/views/Whitelist.js
+++ frontend/src/views/Whitelist.js
@@ -17,12 +17,12 @@ function Whitelist() {
   <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
 </svg>
 </div>
-<div className="removeIcon">
+<div className="List blacklistIcon">
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
   <path strokeLinecap="round" strokeLinejoin="round" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
 </svg>
 </div>
-<div className="blacklistIcon">
+<div className="List removeIcon">
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={3}>
   <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
 </svg>
